e76259b34e4c470d8ee54103a6e981642949ed46
izcoser <76838137+izcoser@users.noreply.github.com>
2023-12-12 16:29:50 -0300

    Modulo 5 - Swagger
    Feito usando Swagger UI Express.

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require("express");
+const { sequelize } = require("./models");
 const {
-  sequelize,
-  insertClient,
-  readClient,
-  updateClient,
-  deleteClient,
-} = require("./models");
+  handleLerCliente,
+  handleAtualizarCliente,
+  handleDeleteCliente,
+  handleCadastrarCliente,
+} = require("./handlers");
 
 const ROUTES = {
   CADASTRAR: "/cadastrarCliente",
@@ -15,112 +15,13 @@ const ROUTES = {
 };
 
 const port = 3000;
-
 const app = express();
-
-const verifyData = (data) => {
-  for (const field of [
-    "nome",
-    "sobrenome",
-    "cpf",
-    "telefone",
-    "logradouro",
-    "numero",
-    "complemento",
-    "bairro",
-    "cidade",
-    "estado",
-    "cep",
-  ]) {
-    if (!(field in data)) {
-      return [false, field];
-    }
-  }
-  return [true, ""];
-};
-
-const handleCadastrarCliente = async (req, res) => {
-  let data = "";
-  req.on("data", (chunk) => {
-    data += chunk;
-  });
-  req.on("end", async () => {
-    try {
-      console.log("Cadastrando cliente...");
-      const fullData = JSON.parse(data);
-      const [validData, missingField] = verifyData(fullData);
-      if (validData) {
-        console.log({ fullData });
-        await insertClient(fullData);
-        res.statusCode = 200;
-        res.setHeader("Content-Type", "text/plain");
-        res.end("Cliente adicionado.");
-      } else {
-        res.statusCode = 400;
-        res.setHeader("Content-Type", "text/plain");
-        res.end(
-          `Campo ${missingField} não encontrado, cliente não pode ser cadastrado.`
-        );
-      }
-    } catch (e) {
-      console.log(e);
-    }
-  });
-};
-
-const handleLerCliente = async (req, res) => {
-  const cpf = req.params.cpf;
-  console.log({ cpf });
-  const client = await readClient(cpf);
-  console.log({ client });
-  if (client) {
-    res.setHeader("Content-Type", "application/json");
-    res.end(JSON.stringify(client.toJSON()));
-  } else {
-    res.setHeader("Content-Type", "text/plain");
-    res.end("Cliente não encontrado.");
-  }
-};
-
-const handleAtualizarCliente = async (req, res) => {
-  let data = "";
-  req.on("data", (chunk) => {
-    data += chunk;
-  });
-  req.on("end", async () => {
-    try {
-      const fullData = JSON.parse(data);
-      console.log({ fullData });
-      const client = await updateClient(fullData.key, fullData);
-      if (client) {
-        res.setHeader("Content-Type", "application/json");
-        res.end(JSON.stringify(client.toJSON()));
-      } else {
-        res.setHeader("Content-Type", "text/plain");
-        res.end("Cliente não encontrado.");
-      }
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "text/plain");
-      res.end("Cliente adicionado.");
-    } catch (e) {
-      console.log(e);
-    }
-  });
-};
-
-const handleDeleteCliente = async (req, res) => {
-  const cpf = req.params.cpf;
-  const success = await deleteClient(cpf);
-  res.setHeader("Content-Type", "text/plain");
-  if (success) {
-    res.end("Cliente deletado.");
-  } else {
-    res.end("Cliente não encontrado.");
-  }
-};
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 (async () => {
   await sequelize.sync();
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
   app.post(ROUTES.CADASTRAR, async (req, res) => {
     await handleCadastrarCliente(req, res);
---
+++ handlers.js
@@ -0,0 +1,113 @@
+const {
+  insertClient,
+  deleteClient,
+  readClient,
+  updateClient,
+} = require("./models");
+
+const verifyData = (data) => {
+  for (const field of [
+    "nome",
+    "sobrenome",
+    "cpf",
+    "telefone",
+    "logradouro",
+    "numero",
+    "complemento",
+    "bairro",
+    "cidade",
+    "estado",
+    "cep",
+  ]) {
+    if (!(field in data)) {
+      return [false, field];
+    }
+  }
+  return [true, ""];
+};
+
+const handleCadastrarCliente = async (req, res) => {
+  let data = "";
+  req.on("data", (chunk) => {
+    data += chunk;
+  });
+  req.on("end", async () => {
+    try {
+      console.log("Cadastrando cliente...");
+      const fullData = JSON.parse(data);
+      const [validData, missingField] = verifyData(fullData);
+      if (validData) {
+        console.log({ fullData });
+        await insertClient(fullData);
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/plain");
+        res.end("Cliente adicionado.");
+      } else {
+        res.statusCode = 400;
+        res.setHeader("Content-Type", "text/plain");
+        res.end(
+          `Campo ${missingField} não encontrado, cliente não pode ser cadastrado.`
+        );
+      }
+    } catch (e) {
+      console.log(e);
+    }
+  });
+};
+
+const handleLerCliente = async (req, res) => {
+  const cpf = req.params.cpf;
+  console.log({ cpf });
+  const client = await readClient(cpf);
+  console.log({ client });
+  if (client) {
+    res.setHeader("Content-Type", "application/json");
+    res.end(JSON.stringify(client.toJSON()));
+  } else {
+    res.setHeader("Content-Type", "text/plain");
+    res.end("Cliente não encontrado.");
+  }
+};
+
+const handleAtualizarCliente = async (req, res) => {
+  let data = "";
+  req.on("data", (chunk) => {
+    data += chunk;
+  });
+  req.on("end", async () => {
+    try {
+      const fullData = JSON.parse(data);
+      console.log({ fullData });
+      const client = await updateClient(fullData.key, fullData);
+      if (client) {
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(client.toJSON()));
+      } else {
+        res.setHeader("Content-Type", "text/plain");
+        res.end("Cliente não encontrado.");
+      }
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "text/plain");
+    } catch (e) {
+      console.log(e);
+    }
+  });
+};
+
+const handleDeleteCliente = async (req, res) => {
+  const cpf = req.params.cpf;
+  const success = await deleteClient(cpf);
+  res.setHeader("Content-Type", "text/plain");
+  if (success) {
+    res.end("Cliente deletado.");
+  } else {
+    res.end("Cliente não encontrado.");
+  }
+};
+
+module.exports = {
+  handleAtualizarCliente,
+  handleDeleteCliente,
+  handleLerCliente,
+  handleCadastrarCliente,
+};
