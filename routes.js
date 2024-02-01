e1a294d8fd909610c1212e4f0f5c2a5dc81a5329
izcoser <76838137+izcoser@users.noreply.github.com>
2024-02-01 16:11:40 -0300

    Use fs.readFile and writeFile async

--- routes.js
+++ routes.js
@@ -1,86 +1,122 @@
-import { readFileSync, writeFileSync } from "node:fs";
+import { readFile, writeFile } from "node:fs";
 
 const filename = "clientes.json";
 const format = "utf8";
 
-const lerClientes = () => {
-  try {
-    const data = readFileSync(filename, { encoding: format });
-    return data;
-  } catch (err) {
-    console.error(err);
-    return `Erro ao ler clientes: ${err}.`;
-  }
+const lerClientes = (res) => {
+  readFile(filename, format, (err, data) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ error: err.message }));
+    } else {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(data);
+    }
+  });
 };
 
-const cadastrarCliente = (cliente) => {
-  try {
-    const data = JSON.parse(readFileSync(filename, { encoding: format }));
-    data.clientes.push(JSON.parse(cliente));
-    writeFileSync(filename, JSON.stringify(data), { encoding: format });
-    return `Cliente cadastrado com sucesso: ${JSON.stringify(cliente)}`;
-  } catch (err) {
-    console.error(err);
-    return `Erro ao cadastrar cliente: ${err}.`;
-  }
-};
-
-const atualizarCliente = (novosDados) => {
-  try {
-    const data = JSON.parse(readFileSync(filename, { encoding: format }));
-    const novosDadosObj = JSON.parse(novosDados);
-    const cliente = data.clientes.find((c) => c.cpf === novosDadosObj.key);
-    if (cliente) {
-      for (const key in novosDadosObj) {
-        if (key in cliente) {
-          cliente[key] = novosDadosObj[key];
+const cadastrarCliente = (cliente, res) => {
+  readFile(filename, format, (err, d) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ error: err.message }));
+    } else {
+      const data = JSON.parse(d);
+      data.clientes.push(JSON.parse(cliente));
+      writeFile(filename, JSON.stringify(data), (err) => {
+        if (err) {
+          res.statusCode = 500;
+          res.setHeader("Content-Type", "application/json");
+          res.end(JSON.stringify({ error: err.message }));
+        } else {
+          res.statusCode = 200;
+          res.setHeader("Content-Type", "application/json");
+          res.end(JSON.stringify(cliente));
         }
-      }
-      writeFileSync(filename, JSON.stringify(data), { encoding: format });
-      return `Cliente atualizado com sucesso: ${JSON.stringify(cliente)}`;
-    } else {
-      return `Cliente com CPF ${novosDadosObj.key} não encontrado.`;
+      });
     }
-  } catch (err) {
-    console.error(err);
-    return `Erro ao atualizar cliente: ${err}.`;
-  }
+  });
 };
 
-const deletarCliente = (cpfCliente) => {
-  try {
-    const data = JSON.parse(readFileSync(filename, { encoding: format }));
-    const newData = data.clientes.filter((c) => c.cpf !== cpfCliente);
-    if (newData.length < data.clientes.length) {
-      data.clientes = newData;
-      writeFileSync(filename, JSON.stringify(data), { encoding: format });
-      return `Cliente com CPF ${cpfCliente} deletado.`;
+const atualizarCliente = (novosDados, res) => {
+  readFile(filename, format, (err, d) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ error: err.message }));
     } else {
-      return `Cliente com CPF ${cpfCliente} não encontrado.`;
+      const data = JSON.parse(d);
+      const novosDadosObj = JSON.parse(novosDados);
+      const cliente = data.clientes.find((c) => c.cpf === novosDadosObj.key);
+
+      if (cliente) {
+        for (const key in novosDadosObj) {
+          if (key in cliente) {
+            cliente[key] = novosDadosObj[key];
+          }
+        }
+
+        writeFile(filename, JSON.stringify(data), (err) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify({ error: err.message }));
+          } else {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify(cliente));
+          }
+        });
+      }
     }
-  } catch (err) {
-    console.error(err);
-    return `Erro ao deletar cliente: ${err}.`;
-  }
+  });
+};
+
+const deletarCliente = (cpfCliente, res) => {
+  readFile(filename, format, (err, d) => {
+    if (err) {
+      res.statusCode = 500;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify({ error: err.message }));
+    } else {
+      const data = JSON.parse(d);
+      const newData = data.clientes.filter((c) => c.cpf !== cpfCliente);
+
+      if (newData.length < data.clientes.length) {
+        data.clientes = newData;
+        writeFile(filename, JSON.stringify(data), (err) => {
+          if (err) {
+            res.statusCode = 500;
+            res.setHeader("Content-Type", "application/json");
+            res.end(JSON.stringify({ error: err.message }));
+          } else {
+            res.statusCode = 200;
+            res.setHeader("Content-Type", "application/json");
+            res.end(
+              JSON.stringify({
+                msg: `Cliente de cpf ${cpfCliente} deletado com sucesso!`,
+              })
+            );
+          }
+        });
+      }
+    }
+  });
 };
 
 const handleRequest = (req, res) => {
   if (req.method === "GET" && req.url === "/ler-clientes") {
-    const contents = lerClientes();
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    console.log(contents);
-    res.end(contents);
+    lerClientes(res);
   } else if (req.method === "POST" && req.url === "/cadastrar-cliente") {
     let cliente = "";
     req.on("data", (chunk) => {
       cliente += chunk;
     });
     req.on("end", () => {
-      const contents = cadastrarCliente(cliente);
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.end(contents);
+      cadastrarCliente(cliente, res);
     });
   } else if (req.method === "PATCH" && req.url === "/atualizar-cliente") {
     let cliente = "";
@@ -88,19 +124,12 @@ const handleRequest = (req, res) => {
       cliente += chunk;
     });
     req.on("end", () => {
-      const contents = atualizarCliente(cliente);
-      res.statusCode = 200;
-      res.setHeader("Content-Type", "application/json");
-      res.end(contents);
+      atualizarCliente(cliente, res);
     });
   } else if (req.method === "DELETE" && req.url.includes("/deletar-cliente")) {
     const cpf = req.url.split("/").at(-1);
-    const contents = deletarCliente(cpf);
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "application/json");
-    res.end(contents);
-  }
-  else{
+    deletarCliente(cpf, res);
+  } else {
     res.statusCode = 404;
     res.setHeader("Content-Type", "application/json");
     res.end("404, rota inexistente.\n");
